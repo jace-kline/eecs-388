918032c9f2c0beea775ee7af94e77ef7e789501d
Jace Kline <jace_kline@ku.edu>
2019-09-15 23:49:02 -0500

    Implementation attempt of lab03

---
+++ lab03/l3-uart/src/eecs388_lib.c
@@ -0,0 +1,122 @@
+#include <stdint.h>
+#include "eecs388_lib.h"
+
+void gpio_mode(int gpio, int mode)
+{
+  uint32_t val;
+  
+  if (mode == OUTPUT) {
+    val = *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_OUTPUT_EN);
+    val |= (1<<gpio);
+    *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_OUTPUT_EN) = val;
+
+    if (gpio == RED_LED || gpio == GREEN_LED || gpio == BLUE_LED) {
+      // active high
+      val = *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_OUTPUT_XOR);
+      val |= (1<<gpio);
+      *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_OUTPUT_XOR) = val;
+    }
+  } else if (mode == INPUT) {
+    val = *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_INPUT_EN);
+    val |= (1<<gpio);
+    *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_INPUT_EN) = val;
+  }
+  return;
+}
+
+void gpio_write(int gpio, int state)
+{
+  uint32_t val = *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_OUTPUT_VAL);
+  if (state == ON) 
+    val |= (1<<gpio);
+  else
+    val &= (~(1<<gpio));    
+  *(volatile uint32_t *) (GPIO_CTRL_ADDR + GPIO_OUTPUT_VAL) = val;
+  return;
+}
+
+inline uint64_t get_cycles(void)
+{
+  return *(volatile uint64_t *)(CLINT_CTRL_ADDR + CLINT_MTIME);
+}
+
+void delay(int msec)
+{
+  uint64_t tend; 
+  tend = get_cycles() + msec * 32768 / 1000;
+  while (get_cycles() < tend) {}; 
+}
+
+void ser_setup()
+{
+  /* initialize UART0 TX/RX */
+  *(volatile uint32_t *)(UART0_CTRL_ADDR + UART_TXCTRL) |= 0x1;
+  *(volatile uint32_t *)(UART0_CTRL_ADDR + UART_RXCTRL) |= 0x1;
+}
+
+/**
+ * write a chacter to the UART 0 FIFO
+ * 
+ * Input: 
+ *  @c    character to send via the UART
+ * Return: None
+ */
+void ser_write(char c)
+{
+  uint32_t regval;
+  /* busy-wait if tx FIFO is full  */
+  do {
+    regval = *(volatile uint32_t *)(UART0_CTRL_ADDR + UART_TXDATA);
+  } while (regval & 0x80000000);
+
+  /* write the character */
+  *(volatile uint32_t *)(UART0_CTRL_ADDR + UART_TXDATA) = c;
+}
+
+/**
+ * write a chacter string to the UART 0
+ * 
+ * Input:
+ *  @str    string point
+ * Return: None
+ */
+void ser_printline(char *str)
+{
+  int i;
+  for (i = 0;; i++) {
+    if (str[i] == '\0') {
+      ser_write('\n');
+      break;
+    }
+    ser_write(str[i]);
+  }
+}
+
+/**
+ * read a chacter from the uart 0
+ * 
+ * Input: None
+ * Return: read byte
+ */
+char ser_read()
+{
+  /*
+    The provided implementation doesn't actually read from the UART 0. 
+
+    What you need to do to implement this function are:
+    1) wait until UART0 RX FIFO is not empty
+    2) read the data from the FIFO and return the read (one) byte. 
+  */
+
+  // A pointer to the RXDATA registry address
+  volatile uint32_t* receive_addr = (volatile uint32_t *)(UART0_CTRL_ADDR + UART_RXDATA);
+  
+  // A variable to store the dereferenced data value
+  uint32_t receive_val;
+  
+  do {
+    receive_val = *(receive_addr);
+  } while(receive_val & 0x80000000); //the condition is true when the 31st bit is true (the queue is empty) -> repeat
+
+  return(receive_val); //return the stored value when the queue is no longer empty
+}

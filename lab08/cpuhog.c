f0ffd673c86d3f06ae54db8a73dca87ff9b993bf
Jace Kline <jace_kline@ku.edu>
2019-10-28 12:45:30 -0500

    Fixed program for lab08

--- lab08/cpuhog.c
+++ lab08/cpuhog.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <sched.h>
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -10,7 +12,7 @@ int main() {
   CPU_SET(0, &s);
   sched_setaffinity(0, sizeof(cpu_set_t), &s);
 
-  int setpriority(PRIO_PROCESS, 0, 5);
+  setpriority(PRIO_PROCESS, 0, 5);
 
   while(1);
 }
